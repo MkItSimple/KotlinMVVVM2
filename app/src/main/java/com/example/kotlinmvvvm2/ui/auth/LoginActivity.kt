0c9f050ef3d1a7bbbf535616f70268f073a20ecd
MkItSimple <choanania1@gmail.com>
2020-03-31 14:48:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kotlinmvvvm2/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinmvvvm2.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlinmvvvm2.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
